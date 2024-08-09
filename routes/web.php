b16ae77b49cf49a7ad89bdd473513de6b9f1d147
Luana Moreira <luanamoreirafm@gmail.com>
2024-08-09 18:50:09 -0300

    up: corrigindo rotas

--- routes/web.php
+++ routes/web.php
@@ -7,23 +7,26 @@ use Illuminate\Support\Facades\Route;
 
 //testes curso
 
-Route::middleware('auth')->group(function () {
-    Route::get('/users/{user}', [UserTester::class, 'show'])->name('users.show');
-    Route::delete('/users/{user}/destroy', [UserTester::class, 'destroy'])->name('users.destroy')->middleware(CheckIfIsAdmin::class);
-    Route::get('/users/{user}/edit', [UserTester::class, 'edit'])->name('users.edit');
-    Route::put('/users/{user}', [UserTester::class, 'update'])->name('users.update');
-    Route::get('/users', function () {
-        return 'luana';
-    });
-    Route::get('/users2', [UserTester::class, 'index'])
-    ->name('users2.index');
-    Route::get('/users3', [UserTester::class, 'index2'])
-    ->name('users3.index2');
-    Route::get('/users4', [UserTester::class, 'index3'])
-    ->name('users4.index3');
-    Route::get('/indexy', [UserTester::class, 'indexy'])->name('indexy.indexy');
-    Route::get('/users/create', [UserTester::class, 'create'])->name('users.create');
-    Route::post('/users/create', [UserTester::class, 'store'])->name('users.store');
+Route::middleware('auth')
+    ->prefix('admin')
+    ->group(function () {
+        //Route::resource('/users', UserTester::class);
+        Route::get('/users/create', [UserTester::class, 'create'])->name('users.create');
+        Route::post('/users/create', [UserTester::class, 'store'])->name('users.store');
+        Route::get('/users/{user}', [UserTester::class, 'show'])->name('users.show');
+        Route::delete('/users/{user}/destroy', [UserTester::class, 'destroy'])->name('users.destroy')->middleware(CheckIfIsAdmin::class);
+        Route::get('/users/{user}/edit', [UserTester::class, 'edit'])->name('users.edit');
+        Route::put('/users/{user}', [UserTester::class, 'update'])->name('users.update');
+        Route::get('/users', function () {
+            return 'luana';
+        });
+        Route::get('/users2', [UserTester::class, 'index'])
+        ->name('users2.index');
+        Route::get('/users3', [UserTester::class, 'index2'])
+        ->name('users3.index2');
+        Route::get('/users4', [UserTester::class, 'index3'])
+        ->name('users4.index3');
+        Route::get('/indexy', [UserTester::class, 'indexy'])->name('indexy.indexy');
 
 });
 
