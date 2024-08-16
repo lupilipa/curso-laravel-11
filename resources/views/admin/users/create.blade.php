3c63bec889551f884e1330e3040efe7edbcbda9d
Júlia Campos <jujubstec@gmail.com>
2024-08-15 21:41:58 -0300

    erro23: tailwind
    resolvo depois

--- resources/views/admin/users/create.blade.php
+++ resources/views/admin/users/create.blade.php
@@ -3,6 +3,10 @@
 
 @section('content')
     <h1>Novo Usuário</h1>
+    @include('admin.alert.partials.')
+    <div class="py" 
+    <h2 class="text-xl font-semibold text-black dark:text-white">Novo Usuario</h2>
+    </div>
     <form action="{{ route('users.store') }}" method="POST">
     <!--input -token ta validando o token da sessao com o do form
     <input type="text" name="_token" value="{{ csrf_token() }}"> -->                
--- resources/views/admin/users/edit.blade.php
+++ resources/views/admin/users/edit.blade.php
@@ -3,6 +3,10 @@
 
 @section('content')
     <h1>Editar o Usuário {{ $user->name }}</h1> 
+    @include('admin.alert.partials')
+    <div class="py" 
+    <h2 class="text-xl font-semibold text-black dark:text-white">Editar Usuario</h2>
+    </div>
     <form action="{{ route('users.update', $user->id) }}" method="POST">        
         @method('put')
         @include('admin.users.partials.form')
